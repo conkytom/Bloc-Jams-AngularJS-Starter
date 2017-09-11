2d820ac120367add6840ac453710fc22c4fda87d
Thomas Conkwright <tmconkwright@gmail.com>
2017-09-11 16:48:16 -0400

    Assignment 7 completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -2,7 +2,12 @@
     function SongPlayer() {
         var SongPlayer = {};
 
+        /**
+        * @desc Currently playing song
+        * @type {Object}
+        */
         var currentSong = null;
+
         /**
         * @desc Buzz object audio file
         * @type {Object}
@@ -28,19 +33,39 @@
             currentSong = song;
         };
 
+        /**
+        * @function playSong
+        * @desc Plays currentBuzzObject and sets playing status to true
+        * to create the proper behavior of the play/pause buttons in album.html
+        * @param {Object} song
+        */
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        };
+
+        /**
+        * @function SongPlayer.play
+        * @desc Plays the currentSong if it is a new song, or coninues playing
+        * the currentSong if it has been paused
+        * @param {Object} song
+        */
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
-                    currentBuzzObject.play();
-                    song.playing = true;
+                    playSong(song);
                 }
             }
         };
 
+        /**
+        * @function SongPlayer.pause
+        * @desc Pauses the currently playing song
+        * @param {Object} song
+        */
         SongPlayer.pause = function(song) {
             currentBuzzObject.pause();
             song.playing = false;
